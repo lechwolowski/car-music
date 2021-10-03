0532b356f7d12dee00ecee1f1cbc96d58f33d12e
Lech Wołowski <lech.wolowski@assecobs.pl>
2021-10-03 16:12:20 +0200

    model improvements

--- model/Model.py
+++ model/Model.py
@@ -1,12 +1,20 @@
 import pickle
+import pandas as pd
+import numpy as np
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 
 
 class Model:
-    def __init__(self, dataset, model_path=None) -> None:
+    def __init__(self, dataset_path, model_path=None) -> None:
+        self.load_dataset(dataset_path=dataset_path)
+        self.load_model(model_path=model_path)
+
+    def load_dataset(self, dataset_path):
+        dataset = pd.read_csv(dataset_path, dtype=int)
+        if dataset.isnull().any(axis=None):
+            raise ValueError("Sparse Dataset")
         self.__dataset = dataset
-        self.__model = self.load_model(model_path=model_path)
 
     def load_model(self, model_path=None):
         if model_path:
@@ -23,7 +31,7 @@ class Model:
                 subsample=0.8,
             )
 
-    def save_model(self, model_path):
+    def save_model(self, model_path="model.pkl"):
         with open(model_path, "wb") as model_file:
             pickle.dump(self.__model, model_file)
 
@@ -32,14 +40,16 @@ class Model:
         Y = self.__dataset.ItemID.values
 
         X_train, _, y_train, _ = train_test_split(
-            X, Y, test_size=0.20, random_state=42, stratify=Y
+            X, Y, test_size=0.20, random_state=1, stratify=Y
         )
 
         self.__model.fit(X_train, y_train)
 
-    def predict(self, args, no_preds):
-        preds = dict(zip(self.__model.classes_, self.__model.predict_proba(args))),
-        sorted_preds = sorted(
-            preds, key=preds.get, reverse=True
-        )
-        return sorted_preds[:no_preds]
+    def predict(self, args, no_preds=5):
+        preds = self.__model.predict_proba(args)
+        classes_preds = [dict(zip(self.__model.classes_, pred)) for pred in preds]
+        sorted_preds = np.array([
+            sorted(cls_pred, key=cls_pred.get, reverse=True)
+            for cls_pred in classes_preds
+        ])
+        return sorted_preds[:,:no_preds]
