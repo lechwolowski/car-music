561610e7bae355a1e41a59f1c2c07bb436ab86fa
Lech Wołowski <lech.wolowski@assecobs.pl>
2021-10-03 23:43:41 +0200

    dockerize

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import pandas as pd
+import train
+import inference
 
-dataset = {
-    "ContextualRating": pd.read_excel("Data_InCarMusic.xlsx", sheet_name="ContextualRating"),
-    "Context Factor": pd.read_excel("Data_InCarMusic.xlsx", sheet_name="Context Factor"),
-    "Music Track": pd.read_excel("Data_InCarMusic.xlsx", sheet_name="Music Track"),
-    "Music Category": pd.read_excel("Data_InCarMusic.xlsx", sheet_name="Music Category"),
-}
 
-print(dataset)
+def main():
+    train.main()
+    while True:
+        input_data = inference.input_gathering()
+        inference.inference(input_data)
+        if input("type \"exit\" to exit: ") == "exit":
+            break
+    
+
+if __name__ == "__main__":
+    main()
